325afe0920cf2d63d45872223a58205d9ca66df8
Le Minh Son <son.minh@mobifone.vn>
2024-02-21 17:07:42 +0700

    add firebase

--- src/components/_pages/trang-chu/index.tsx
+++ src/components/_pages/trang-chu/index.tsx
@@ -1,27 +1,35 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Theme } from '@emotion/react';
+import { Box, Grid, SxProps, Typography } from '@mui/material';
 import React from 'react';
 
 const Banner = () => {
   return (
-    <Box>
+    <Box sx={BannerContainer}>
       <Grid
         container
-        sx={{ display: 'grid', gridTemplateColumns: 'auto auto' }}>
-        <Grid item sx={{ gridRow: '1/ span 2', bgcolor: 'pink' }}>
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: 'auto auto',
+          height: '100%',
+        }}>
+        <Grid
+          item
+          sx={{ gridRow: '1/ span 2', bgcolor: 'pink' }}
+          className='article-item'>
           <Typography>
             Front-end developer lên trình như thế nào? Bạn đã thực sự hiểu về
             Front-End?
           </Typography>
           <Typography>February 16, 2024</Typography>
         </Grid>
-        <Grid item sx={{ bgcolor: 'pink' }}>
+        <Grid item sx={{ bgcolor: 'pink' }} className='article-item'>
           <Typography>
             Front-end developer lên trình như thế nào? Bạn đã thực sự hiểu về
             Front-End?
           </Typography>
           <Typography>February 16, 2024</Typography>
         </Grid>
-        <Grid item sx={{ bgcolor: 'pink' }}>
+        <Grid item sx={{ bgcolor: 'pink' }} className='article-item'>
           <Typography>
             Front-end developer lên trình như thế nào? Bạn đã thực sự hiểu về
             Front-End?
@@ -34,3 +42,13 @@ const Banner = () => {
 };
 
 export default Banner;
+
+const BannerContainer: SxProps<Theme> = {
+  height: 300,
+  '.article-item': {
+    background:
+      'url("https://topdev.vn/blog/wp-content/uploads/2020/08/Thumnail-324x235.jpg")',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+};
---
+++ src/ultis/FirebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDkcZJC6vxDyyHEbQXhbPdmn8MQ5T7Yhbg",
+  authDomain: "dev-blog-72283.firebaseapp.com",
+  projectId: "dev-blog-72283",
+  storageBucket: "dev-blog-72283.appspot.com",
+  messagingSenderId: "864169795199",
+  appId: "1:864169795199:web:bf85d3b56c11c68594e94a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

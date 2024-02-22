f650ab95e39c180d81d044466883db02f5c6d749
Son Le <sonlele2000@gmail.com>
2024-02-22 21:43:05 +0700

    update

--- src/components/_pages/trang-chu/index.tsx
+++ src/components/_pages/trang-chu/index.tsx
@@ -30,25 +30,52 @@ const Banner = () => {
               />
             </Link>
           </Box>
-          <Typography>
-            Front-end developer lên trình như thế nào? Bạn đã thực sự hiểu về
-            Front-End?
-          </Typography>
-          <Typography>February 16, 2024</Typography>
+          <Box className='article-item-info'>
+            <Typography>
+              Front-end developer lên trình như thế nào? Bạn đã thực sự hiểu về
+              Front-End?
+            </Typography>
+            <Typography>February 16, 2024</Typography>
+          </Box>
         </Grid>
         <Grid item sx={{ bgcolor: 'pink' }} className='article-item'>
-          <Typography>
-            Front-end developer lên trình như thế nào? Bạn đã thực sự hiểu về
-            Front-End?
-          </Typography>
-          <Typography>February 16, 2024</Typography>
+          <Box className='article-item-thumb'>
+            <Link href={'/'}>
+              <Image
+                src={
+                  'https://firebasestorage.googleapis.com/v0/b/dev-blog-7a694.appspot.com/o/cau-hoi-cho-nha-tuyen-dung-it-534x462.png?alt=media&token=cae6b033-a0cd-4a06-a24f-f5a60c0691ac'
+                }
+                fill
+                alt='cc'
+              />
+            </Link>
+          </Box>
+          <Box className='article-item-info'>
+            <Typography>
+              Top 30+ câu hỏi cho nhà tuyển dụng IT khi phỏng vấn
+            </Typography>
+            <Typography>February 20, 2024</Typography>
+          </Box>
         </Grid>
         <Grid item sx={{ bgcolor: 'pink' }} className='article-item'>
-          <Typography>
-            Front-end developer lên trình như thế nào? Bạn đã thực sự hiểu về
-            Front-End?
-          </Typography>
-          <Typography>February 16, 2024</Typography>
+          <Box className='article-item-thumb'>
+            <Link href={'/'}>
+              <Image
+                src={
+                  'https://firebasestorage.googleapis.com/v0/b/dev-blog-7a694.appspot.com/o/cau-hoi-cho-nha-tuyen-dung-it-534x462.png?alt=media&token=cae6b033-a0cd-4a06-a24f-f5a60c0691ac'
+                }
+                fill
+                alt='cc'
+              />
+            </Link>
+          </Box>
+          <Box className='article-item-info'>
+            <Typography>
+              Front-end developer lên trình như thế nào? Bạn đã thực sự hiểu về
+              Front-End?
+            </Typography>
+            <Typography>February 16, 2024</Typography>
+          </Box>
         </Grid>
       </Grid>
     </Box>
@@ -63,69 +90,50 @@ const BannerContainer: SxProps<Theme> = {
     position: 'relative',
     overflow: 'hidden',
     '.article-item-thumb': {
-      position: 'absolute',
-      left: '-9999px',
-      right: '-9999px',
-      width: 'auto',
-      margin: '0 auto',
-      objectFit: 'cover',
-      transitionTimingFunction: 'ease !important',
-      transition: 'transform 0.3s ease, opacity 0.3s',
-      WebkitTransition: '-webkit-transform 0.3s ease, opacity 0.3s',
+      '& a': {
+        ':before': {
+          bottom: '0',
+          content: '""',
+          display: 'block',
+          height: '100%',
+          width: '100%',
+          position: 'absolute',
+          WebkitTransition: 'background-color 0.3s ease',
+          transition: 'background-color 0.3s ease',
+          zIndex: 1,
+          background:
+            'linear-gradient(to bottom, rgba(0, 0, 0, 0) 40%, rgba(0, 0, 0, 0.7) 100%)',
+        },
+        '& img': {
+          objectFit: 'cover',
+          transition: 'transform 0.3s ease, opacity 0.3s',
+          WebkitTransition: '-webkit-transform 0.3s ease, opacity 0.3s',
+        },
+      },
     },
-    '&::before': {
-      bottom: '0',
-      content: '""',
-      display: 'block',
-      height: '100%',
+    '.article-item-info': {
+      position: 'absolute',
+      bottom: 0,
       width: '100%',
-      position: 'absolute',
-      // backgroundColor: 'rgba(0, 0, 0, 0.1)',
-      WebkitTransition: 'background-color 2s ease',
-      transition: 'background-color 2s ease',
       zIndex: 1,
-      background:
-        'linear-gradient(to bottom, rgba(0, 0, 0, 0) 40%, rgba(0, 0, 0, 0.7) 100%)',
-      // bottom: '0',
-      // content: '""',
-      // display: 'block',
-      // height: '70%',
-      // width: '100%',
-      // position: 'absolute',
-      // WebkitTransition: 'background-color 2s ease',
-      // transition: 'background-color 2s ease',
-      // zIndex: 1,
-      // background:
-      //   'linear-gradient(to bottom, rgba(0, 0, 0, 0) 40%, rgba(0, 0, 0, 0.7) 100%)',
     },
-    // '&:hover::before': {
-    //   // bottom: '0',
-    //   // content: '""',
-    //   // display: 'block',
-    //   // height: '100%',
-    //   // width: '100%',
-    //   // position: 'absolute',
-    //   backgroundColor: 'rgba(0, 0, 0, 0.1)',
-    //   // WebkitTransition: 'background-color 2s ease',
-    //   // transition: 'background-color 2s ease',
-    // },
     ':hover': {
       bgcolor: 'pink',
       ':before': {
         bgcolor: 'pink',
-        // bottom: '0',
-        // content: '""',
-        // display: 'block',
-        // height: '100%',
-        // width: '100%',
-        // position: 'absolute',
+
         backgroundColor: 'rgba(0, 0, 0, 0.1)',
-        // WebkitTransition: 'background-color 2s ease',
-        // transition: 'background-color 2s ease',
       },
       '.article-item-thumb': {
-        // transform: 'scale3d(1.1, 1.1, 1)',
-        // WebkitTransform: 'scale3d(1.1, 1.1, 1)',
+        '& a': {
+          ':before': {
+            backgroundColor: 'rgba(0, 0, 0, 0.1)',
+          },
+          '& img': {
+            transform: 'scale3d(1.1, 1.1, 1)',
+            WebkitTransform: 'scale3d(1.1, 1.1, 1)',
+          },
+        },
       },
     },
   },

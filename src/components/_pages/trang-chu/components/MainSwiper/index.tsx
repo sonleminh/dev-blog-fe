6664cb401ca77f41d23a4106b29e3092379bf0b8
Son Le <sonlele2000@gmail.com>
2024-02-29 23:49:59 +0700

    update banner

--- src/components/_pages/trang-chu/components/MainSwiper/index.tsx
+++ src/components/_pages/trang-chu/components/MainSwiper/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import { Autoplay, FreeMode, Thumbs } from 'swiper/modules';
 import { Box, SxProps, Theme } from '@mui/material';
 import SkeletonImage from '@/components/common/SkeletonImage';
 
@@ -9,7 +9,6 @@ const MainSwiper = (props: any) => {
     <Box sx={MainSwiperStyle}>
       <Swiper
         spaceBetween={10}
-        navigation={true}
         thumbs={{
           swiper:
             props?.thumbsSwiper && !props?.thumbsSwiper?.destroyed
@@ -17,11 +16,11 @@ const MainSwiper = (props: any) => {
               : null,
         }}
         loop={true}
-        // autoplay={{
-        //   delay: 1500,
-        //   disableOnInteraction: false,
-        // }}
-        modules={[Autoplay, FreeMode, Navigation, Thumbs]}
+        autoplay={{
+          delay: 5500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay, FreeMode, Thumbs]}
         className='mainSwiper'>
         {props?.data?.map((item: any) => (
           <SwiperSlide key={item}>
@@ -31,19 +30,12 @@ const MainSwiper = (props: any) => {
                 width: '100%',
                 height: { xs: '160px', sm: '160px', md: '100%' },
                 mb: 1,
-                // borderRadius: '8px',
                 overflow: 'hidden',
                 '& img': {
                   objectFit: 'cover',
                 },
               }}>
-              <SkeletonImage
-                src={
-                  'https://firebasestorage.googleapis.com/v0/b/dev-blog-7a694.appspot.com/o/cau-hoi-cho-nha-tuyen-dung-it-534x462.png?alt=media&token=cae6b033-a0cd-4a06-a24f-f5a60c0691ac'
-                }
-                alt='cc'
-                fill
-              />
+              <SkeletonImage src={item.thumbnail} alt='cc' fill />
             </Box>
           </SwiperSlide>
         ))}
@@ -56,6 +48,7 @@ export default MainSwiper;
 
 const MainSwiperStyle: SxProps<Theme> = {
   '.mainSwiper': {
-    height: 400,
+    height: 450,
+    borderRadius: '10px',
   },
 };
--- src/components/_pages/trang-chu/components/ThumbSwiper/index.tsx
+++ src/components/_pages/trang-chu/components/ThumbSwiper/index.tsx
@@ -1,52 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {
-  Autoplay,
-  FreeMode,
-  Navigation,
-  Pagination,
-  Thumbs,
-} from 'swiper/modules';
+import { Autoplay, FreeMode, Navigation, Thumbs } from 'swiper/modules';
 import SkeletonImage from '@/components/common/SkeletonImage';
 import { Box, SxProps, Theme } from '@mui/material';
 
 const ThumbSwiper = (props: any) => {
+  const progressCircle = useRef<SVGSVGElement>(null);
+  const progressContent = useRef<HTMLSpanElement>(null);
+  const onAutoplayTimeLeft = (_, time: any, progress: any) => {
+    if (progressCircle.current) {
+      progressCircle.current.style.setProperty('--progress', `${1 - progress}`);
+    }
+    if (progressContent.current) {
+      progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  };
+  // console.log('1:', progressCircle);
+  // console.log('2:', progressContent);
   return (
-    // <Box sx={SwiperStyle}>
-    //   <Swiper
-    //     direction={'vertical'}
-    //     // autoHeight={true}
-    //     slidesPerView={2}
-    //     pagination={{
-    //       clickable: true,
-    //     }}
-    //     modules={[Pagination]}
-    //     className='mySwiper'>
-    //     <SwiperSlide>
-    //       <Box sx={{ width: '100%', height: 300, bgcolor: 'pink' }}>
-    //         Slide 1
-    //       </Box>
-    //     </SwiperSlide>
-    //     <SwiperSlide>
-    //       <Box sx={{ width: '100%', height: 300, bgcolor: 'green' }}>
-    //         Slide 2
-    //       </Box>
-    //     </SwiperSlide>
-    //     <SwiperSlide>
-    //       <Box sx={{ width: '100%', height: 300, bgcolor: 'pink' }}>
-    //         Slide 3
-    //       </Box>
-    //     </SwiperSlide>
-    //     <SwiperSlide>
-    //       <Box sx={{ width: '100%', height: 300, bgcolor: 'green' }}>
-    //         Slide 4
-    //       </Box>
-    //     </SwiperSlide>
-    //     <SwiperSlide>
-    //       <Box sx={{ width: '100%', height: 300, bgcolor: 'red' }}>Slide 5</Box>
-    //     </SwiperSlide>
-    //   </Swiper>
-    // </Box>
     <Box sx={SwiperStyle}>
       <Swiper
         onSwiper={props?.setThumbsSwiper}
@@ -55,40 +26,39 @@ const ThumbSwiper = (props: any) => {
         spaceBetween={10}
         slidesPerView={4}
         freeMode={true}
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
         watchSlidesProgress={true}
         loop={true}
-        // autoplay={{
-        //   delay: 1500,
-        //   disableOnInteraction: false,
-        // }}
+        autoplay={{
+          delay: 5500,
+          disableOnInteraction: false,
+        }}
         pagination={{
           clickable: true,
         }}
-        // modules={[Pagination]}
-        // modules={[FreeMode, Navigation, Thumbs]}
         modules={[Autoplay, FreeMode, Navigation, Thumbs]}
         className='mySwiper'>
         {props?.data?.map((item: any) => (
           <SwiperSlide key={item}>
-            <Box
-              sx={{
-                position: 'relative',
-                width: '100%',
-                height: { xs: '160px', sm: '160px', md: '160px' },
-                mb: 1,
-                borderRadius: '8px',
-                overflow: 'hidden',
-                '& img': {
-                  objectFit: 'cover',
-                },
-              }}>
-              <SkeletonImage
-                src={
-                  'https://firebasestorage.googleapis.com/v0/b/dev-blog-7a694.appspot.com/o/cau-hoi-cho-nha-tuyen-dung-it-534x462.png?alt=media&token=cae6b033-a0cd-4a06-a24f-f5a60c0691ac'
-                }
-                alt='cc'
-                fill
-              />
+            <Box className='thumb-item'>
+              <Box
+                className='thumb-container'
+                sx={{
+                  position: 'relative',
+                  width: { xs: '100%' },
+                  height: { xs: '100%' },
+                  mb: 1,
+                  borderRadius: '50%',
+                  overflow: 'hidden',
+                  '& img': {
+                    objectFit: 'cover',
+                  },
+                }}>
+                <SkeletonImage src={item.thumbnail} alt='cc' fill />
+              </Box>
+              <svg className='progressBar'>
+                <circle r='35' cx='36' cy='36'></circle>
+              </svg>
             </Box>
           </SwiperSlide>
         ))}
@@ -106,7 +76,61 @@ const SwiperStyle: SxProps<Theme> = {
   right: 0,
   zIndex: 2,
   '.mySwiper': {
-    width: 100,
-    height: 300,
+    width: 120,
+    height: 350,
+  },
+  '.swiper-slide': {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  '.thumb-item': {
+    position: 'relative',
+    width: '72px',
+    height: '72px',
+  },
+  '.thumb-container': {
+    ':before': {
+      content: '""',
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      width: '100%',
+      height: '100%',
+      boxShadow: '0 0 0 3px inset rgba(255,255,255,.28)',
+      zIndex: '9',
+      borderRadius: '50%',
+    },
+  },
+  '.progressBar': {
+    display: 'none',
+    position: 'absolute',
+    top: '0',
+    right: '0',
+    width: '72px',
+    height: '72px',
+    '& circle': {
+      fill: 'rgba(255,255,255,0)',
+      stroke: '#fff',
+      strokeWidth: '3px',
+      strokeLinecap: 'round',
+      animation: 'progress 5.5s',
+      strokeDasharray: '219.8',
+      strokeDashoffset: '219.8',
+    },
+  },
+  '.swiper-slide-active .progressBar': {
+    display: 'block',
+  },
+  '@keyframes progress': {
+    '0%': {
+      strokeDashoffset: '219.8',
+    },
+    '100%': {
+      strokeDashoffset: '0',
+    },
   },
 };

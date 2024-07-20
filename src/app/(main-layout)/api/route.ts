7dd553d07166a8e65bdc3834a499ec93b74ce95a
Son Le <sonlele2000@gmail.com>
2024-07-21 00:34:14 +0700

    remove api file

--- src/app/(main-layout)/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { getRequest } from "@/utils/fetch-client";
-
-export async function GET() {
-    const data = await 
-      getRequest(
-        `http://localhost:8080/admin/api/article?find_option=HOME`
-      )
-    return Response.json({ data }) 
-  }

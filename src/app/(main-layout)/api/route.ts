e2201ce323830039251c340e662dab049d9d8930
Son Le <sonlele2000@gmail.com>
2024-07-21 00:59:07 +0700

    replace real api

--- src/app/(main-layout)/api/route.ts
+++ src/app/(main-layout)/api/route.ts
@@ -5,7 +5,7 @@ const PUBLIC_URL = process.env.NEXT_PUBLIC_HOST;
     
     const data = await 
       getRequest(
-        `${PUBLIC_URL}article?find_option=HOME`
+        `https://dev-blog-be-production.up.railway.app/admin/api/article?find_option=HOME`
       )
     return Response.json({ data }) 
   }

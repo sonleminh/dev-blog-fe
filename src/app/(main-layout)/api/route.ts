8e5598b28a1b2e4bd6a7b5d39fa56b443631a250
Son Le <sonlele2000@gmail.com>
2024-07-21 21:03:49 +0700

    fix

--- src/app/(main-layout)/api/route.ts
+++ src/app/(main-layout)/api/route.ts
@@ -2,6 +2,9 @@ import { BASE_API_URL } from "@/constants/env";
 import { getRequest } from "@/utils/fetch-client";
 
 export async function GET() {
+  if (!BASE_API_URL) {
+    return null
+  }
     const data = await 
       getRequest(
         `${BASE_API_URL}/article?find_option=HOME`

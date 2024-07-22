a47ca6dca3b7347838d796690b4f8818a263062b
Son Le <sonlele2000@gmail.com>
2024-07-23 00:19:09 +0700

    add try catch and log error

--- src/app/(main-layout)/api/route.ts
+++ src/app/(main-layout)/api/route.ts
@@ -2,9 +2,14 @@ import { getRequest } from "@/utils/fetch-client";
 import { BASE_API_URL } from "@/constants/env";
 
 export async function GET() {
-    const data = await 
+    try {
+      const data = await 
       getRequest(
         `${BASE_API_URL}/article?find_option=HOME`
       )
     return Response.json({ data }) 
+    } catch (error) {
+      console.error('Error in GET route handler:', error);
+      return Response.json({ error: 'Failed to fetch data' }, { status: 500 });
+    }
   }
--- src/app/(main-layout)/tag/[tag]/api/route.ts
+++ src/app/(main-layout)/tag/[tag]/api/route.ts
@@ -5,8 +5,13 @@ import { BASE_API_URL } from "@/constants/env";
 export async function GET(request: NextRequest, {params}: {
     params: { tag: string}
 }) {
-    const searchParams = request.nextUrl.searchParams
-    const page = searchParams.get('page')
-    const data = await getRequest(`${BASE_API_URL}/article/tag/${params.tag}?page=${page}&limit=10`)
-    return Response.json({data})
+    try {
+        const searchParams = request.nextUrl.searchParams
+        const page = searchParams.get('page')
+        const data = await getRequest(`${BASE_API_URL}/article/tag/${params.tag}?page=${page}&limit=10`)
+        return Response.json({data})
+    } catch (error) {
+        console.error('Error in GET route handler:', error);
+        return Response.json({ error: 'Failed to fetch articles by tag' }, { status: 500 });
+    }
 }
